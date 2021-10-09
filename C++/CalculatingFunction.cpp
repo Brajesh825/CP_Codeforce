8a0128b18974276b6b206819d45aed750269c172
Brajesh825 <brajeshmishra825@gmail.com>
2021-10-09 15:05:39 +0530

    Calculating Function modified

--- C++/CalculatingFunction.cpp
+++ C++/CalculatingFunction.cpp
@@ -1,24 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int calculatingFunction(int n)
-{
-	int count=0;
-
-	for(unsigned i = 1; i <= n; ++i) {
-		if(i%2==1)
-			count-=i;
-		else
-			count+=i;
-
-	}
-	return count;
-}
-
 int main()
 {
-	int n;
-	cin>>n;
-	n=calculatingFunction(n);
-	cout<<n;
+	long long n, a;
+    cin >> n;
+    if(n % 2 == 0) a = n / 2;
+    else a = ((n + 1) / 2) * (-1);
+    cout << a << endl;
+    return 0;
 }
