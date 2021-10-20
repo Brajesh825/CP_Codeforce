306f07279eec16f559a28d069e2591517154e3b5
Brajesh825 <brajeshmishra825@gmail.com>
2021-10-20 13:05:22 +0530

    1370A - Maximum GCD Challenge Completed

---
+++ C++/800/MaximumGCD.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fastread() (ios_base::sync_with_stdio(false),cin.tie(NULL));
+
+void fastscan(int &x)
+{
+        bool neg=false;
+        register int c;
+        x =0;
+        c=getchar();
+        if(c=='-')
+        {
+            neg = true;
+            c=getchar();
+        }
+        for(;(c>47 && c<58);c=getchar())
+            x = (x<<1) + (x<<3) +c -48;
+        if(neg)
+            x *=-1;
+}
+int main()
+{
+    fastread();
+    int t;
+    fastscan(t);
+    while(t--)
+    {
+        int n;
+        fastscan(n);
+
+        int gcd = n/2;
+        cout<<gcd<<'\n';
+
+    }
+}
