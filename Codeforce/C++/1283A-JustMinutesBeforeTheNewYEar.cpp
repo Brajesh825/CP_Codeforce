f55b91d0f84ab75168253278f4eafda39833c1de
Brajesh825 <brajeshmishra825@gmail.com>
2021-11-04 17:27:09 +0530

    1283A- Just minutes before the new year completed

---
+++ Codeforce/C++/1283A-JustMinutesBeforeTheNewYEar.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vll;
+#define pno cout<<"NO"<<'\n';
+#define pyes cout<<"YES"<<'\n';  
+
+#define log(x) std::cout << #x"=" << x << std::endl
+
+#define CHIDORI (ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL));
+
+
+int main()
+{
+    CHIDORI
+    int t;
+    cin>>t;
+
+    while(t--){
+      int h,m;
+      cin>>h>>m;
+
+      cout<<(24-h)*60-m<<'\n';
+    }
+
+    return 0;
+}
