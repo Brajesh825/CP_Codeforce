dadbb74f5bfb4203b7f30bad21206fd72d62e786
Brajesh825 <brajeshmishra825@gmail.com>
2021-11-06 16:29:05 +0530

    1372A- Omkar and Completion completed

---
+++ Codeforce/C++/1372A-OmkarAndCompletion.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vll;
+#define pno cout<<"NO"<<'\n';
+#define pyes cout<<"YES"<<'\n';  
+#define log(x) std::cout << #x"=" << x << std::endl
+#define CHIDORI (ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL));
+
+int main()
+{
+    CHIDORI
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+
+        for (int i = 0; i < n; ++i)
+        {
+            cout<<1000<<' ';
+        }
+        cout<<'\n';
+    }
+}
