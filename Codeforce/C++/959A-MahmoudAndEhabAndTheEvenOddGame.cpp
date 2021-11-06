2b687fcfefbdbaca4f81af1a72385a6a25295c5d
Brajesh825 <brajeshmishra825@gmail.com>
2021-11-06 15:31:28 +0530

    959A- Mahmoud and Ehab and the even-odd game completed

---
+++ Codeforce/C++/959A-MahmoudAndEhabAndTheEvenOddGame.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vll;
+#define pno cout<<"NO"<<'\n';
+#define pyes cout<<"YES"<<'\n';  
+
+#define log(x) std::cout << #x"=" << x << std::endl
+
+#define CHIDORI (ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL));
+
+
+int main()
+{
+    CHIDORI
+
+    int n;
+    cin>>n;
+
+
+
+    if(n%2==0)
+    {
+        cout<<"Mahmoud";
+    }else{
+        cout<<"Ehab";
+    }
+
+    
+}
