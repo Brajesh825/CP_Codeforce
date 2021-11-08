db0b0aac85ffe436068da9ae52635d7125e3dd02
Brajesh825 <brajeshmishra825@gmail.com>
2021-11-08 21:36:30 +0530

    Deleting and inserting last node of linkedList

---
+++ GeeksforGeeks/LinkedList/C++/DeleteFirstNodeOfALinkedList.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+	int data;
+	Node* next;
+
+	Node(int x)
+	{
+		data=x;
+		next=NULL;	
+	}
+};
+
+void printList(Node* head)
+{
+	Node * curr = head;
+	while(curr!=NULL)
+	{
+		cout<<curr->data<<' ';
+		curr=curr->next;
+	}
+	cout<<'\n';
+}
+
+Node* insertAtTheBeginning(Node* head,int data)
+{
+	Node* temp = new Node(data);
+	temp->next=head;
+	return temp;
+}
+
+Node* insertAtTheEnd(Node* head,int data)
+{
+	if(head==NULL)
+	{
+		head = new Node(data);
+		return head;
+	}
+	else
+	{
+		Node* temp=head;
+		while(temp->next!=NULL)
+			temp=temp->next;
+
+		temp->next= new Node(data);
+		return head;
+	}
+}
+
+Node* deleteFirstNode(Node* head)
+{
+	if(head==NULL){
+		return head;
+	}
+	else{
+		Node* temp=head->next;
+		delete head;
+		return temp;
+	}
+}
+
+int main()
+{
+	Node* head = NULL;
+	head=insertAtTheEnd(head,10);
+	head=insertAtTheEnd(head,20);
+	head=insertAtTheEnd(head,30); 
+	head=deleteFirstNode(head);
+	printList(head);
+	head=deleteFirstNode(head);
+	printList(head);
+	head=deleteFirstNode(head);
+	printList(head);
+	return 0; 
+}
